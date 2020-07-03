54c753a940a765dcf59daee57fb802e47675ca67
midapsh <34818053+midapsh@users.noreply.github.com>
2020-07-03 18:42:20 -0300

    Add timer to Toast with component lifecycle

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
 import { useToast, ToastMessage } from '../../../hooks/toast';
@@ -10,6 +10,17 @@ interface ToastProps {
 }
 const Toast: React.FC<ToastProps> = ({ message }) => {
   const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, message.id]);
+
   return (
     <Container
       type={message.type}
